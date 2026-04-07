75261db778d29c8fb544e1818ef96fb084d0e446
Gabriel Njoabozia <flawlexsaint@users.noreply.github.com>
2026-04-07 15:38:51 +0100

    feat: remove AreasSection from homepage and delete component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { CTASection } from "@/components/sections/CTASection";
 import { HeroSection } from "@/components/sections/HeroSection";
 import { BlogSection } from "@/components/sections/BlogSection";
 import { ServicesSection } from "@/components/sections/ServicesSection";
-import { AreasSection } from "@/components/sections/AreasSection";
 import { TestimonialsSection } from "@/components/sections/TestimonialsSection";
 
 export default function HomePage() {
@@ -14,7 +13,6 @@ export default function HomePage() {
       <main>
         <HeroSection />
         <ServicesSection />
-        <AreasSection />
         <TestimonialsSection />
         <BlogSection />
         <CTASection />
